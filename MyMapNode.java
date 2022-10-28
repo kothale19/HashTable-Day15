2ac06077993076669b73e83087b3d99a2c164122
kothale19 <109896578+kothale19@users.noreply.github.com>
2022-10-28 11:39:18 +0530

    Add files via upload
    HashTable

---
+++ MyMapNode.java
@@ -0,0 +1,34 @@
+package com.blz.hashtable;
+
+public class MyMapNode<K,V> {
+	
+	
+	
+	
+	 K key;
+	    V value;
+	    MyMapNode<K,V> next;
+
+	    public MyMapNode(K key, V value) {
+	        this.key = key;
+	        this.value = value;
+	    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  @Override
+		    public String toString() {
+		        return "MyMapNode{" +
+		                "key=" + key +
+		                ", value=" + value +
+		                '}';
+		
+		
+		
+
+	}
+
+}
+}
+}
